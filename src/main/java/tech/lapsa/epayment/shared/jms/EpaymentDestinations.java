9cc888939e615fddc3b3158f39a94fa2c55f7c09
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-11-29 10:03:59 +0600

    set params TODOs

--- src/main/java/tech/lapsa/epayment/shared/jms/EpaymentDestinations.java
+++ src/main/java/tech/lapsa/epayment/shared/jms/EpaymentDestinations.java
@@ -5,7 +5,16 @@ public final class EpaymentDestinations {
     private EpaymentDestinations() {
     }
 
+    // TODO : set params
+    static {
+	init();
+    }
+
+    static void init() {
+	throw new RuntimeException("Parameters must set");
+    }
+
     public static final String ACCEPT_INVOICE = "";
     public static final String PAYMENT_URI_QUALIFIER = "";
-    
+
 }
