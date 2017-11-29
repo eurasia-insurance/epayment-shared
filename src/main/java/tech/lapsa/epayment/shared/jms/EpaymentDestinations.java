fc271c1c015d98c99482673c31196e51f3698f4f
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-11-29 09:14:19 +0600

    destination constants

---
+++ src/main/java/tech/lapsa/epayment/shared/jms/EpaymentDestinations.java
@@ -0,0 +1,10 @@
+package tech.lapsa.epayment.shared.jms;
+
+public final class EpaymentDestinations {
+
+    private EpaymentDestinations() {
+    }
+
+    public static final String ACCEPT_INVOICE = "";
+    
+}
