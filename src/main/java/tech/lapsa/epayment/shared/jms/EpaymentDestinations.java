835863363519c55c691a21e77183aeef739bc074
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-12-20 09:23:55 +0600

    not using jms

--- src/main/java/tech/lapsa/epayment/shared/jms/EpaymentDestinations.java
+++ src/main/java/tech/lapsa/epayment/shared/jms/EpaymentDestinations.java
@@ -5,9 +5,6 @@ public final class EpaymentDestinations {
     private EpaymentDestinations() {
     }
 
-    public static final String ACCEPT_INVOICE = "epayment/jms/calls/acceptInvoice";
-    public static final String SPECIFY_PAYMENT_URI = "epayment/jms/calls/specifyPaymentURI";
-    public static final String COMPLETE_PAYMENT_WITH_UNKNOWN_TYPE = "epayment/jms/calls/completePaymentWithUnknownType";
     public static final String INVOICE_HAS_PAID = "epayment/jms/events/invoicesHasPaid";
 
     public static final String NOTIFIER_PAYMENTSUCCESS_REQUESTER_EMAIL = "epayment/jms/events/notifier/paymentSuccessUserEmail";
